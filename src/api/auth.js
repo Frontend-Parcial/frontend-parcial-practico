a15fdc66d35e5b43cc9db96520fa51b866bc24bb
danielDiloq <daniel@diloq.com>
2025-05-06 14:38:05 -0500

    --agregar ciertas conexiones de los endpoint

---
+++ src/api/auth.js
@@ -0,0 +1,23 @@
+import axiosClient from "./axiosclient";
+
+
+export const register = (email, password, name, role) => {
+    const registerData = {
+        email: email,
+        password: password,
+        nombre: name,
+        rol: role
+    };
+
+    return axiosClient.post('/auth/register', registerData);
+};
+
+
+export const login = (email, password) => {
+    const loggingData = {
+        email: email,
+        password: password
+    };
+
+    return axiosClient.post('auth/login', loggingData);
+};
