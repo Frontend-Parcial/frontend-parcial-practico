3850a94e9af28a18d520b36c0f59286319336bcf
jhogerolmos <jhogerolmos@gmail.com>
2025-05-28 20:15:37 -0500

    convenios

--- src/App.jsx
+++ src/App.jsx
@@ -16,10 +16,11 @@ import { HealthCheck } from './pages/healthcheck'
 import ListaSolicitudes from './pages/solicitudes/ListaSolicitudes'
 import CrearSeguimiento from './pages/seguimiento/gestionar-seguimiento'
 import SolicitudIntercambioForm from './pages/solicitudes/SolicitudesCreate'
-
-import { CrearSeguimiento } from './pages/seguimiento/gestionar-seguimiento'
 import { AnimatePresence } from 'framer-motion'
 import Layout from './components/Layout'
+import ListaConvenios from './pages/convenios/ListarConvenios'
+import CrearConvenio from './pages/convenios/component/CrearNuevoConvenio'
+import { DetalleConvenio } from './pages/convenios/DetalleConvenio'
 
 function AnimatedRoutes() {
   const location = useLocation()
@@ -41,11 +42,16 @@ function AnimatedRoutes() {
           <Route path='/docentes/:id' element={<Layout><ObtenerDocentes /></Layout>} />
           <Route path='/docentes/actualizar/:id' element={<Layout><ActualizarDocentes /></Layout>} />
           <Route path='/solicitudes' element={<Layout><ListaSolicitudes /></Layout>} />
+          <Route path='/solicitudes/nuevo' element={<Layout><SolicitudIntercambioForm /></Layout>} />
+          <Route path='/convenios' element={<Layout><ListaConvenios /></Layout>} />
+          <Route path='/convenios/nuevo' element={<Layout><CrearConvenio /></Layout>} />
+          <Route path='/convenios/:id' element={<Layout><DetalleConvenio /></Layout>} />
+
           <Route path='/seguimiento' element={<Layout><CrearSeguimiento /></Layout>} />
           <Route path='/seguimiento/crear' element={<Layout><SolicitudIntercambioForm /></Layout>} />
           {/* <Route path='/prueba' element={<Pruebas />} /> */}
           {/* <Route path='/solicitudes' element={<ListaSolicitudes />} /> */}
-          {/* <Route path='/solicitudes/nuevo' element={<SolicitudIntercambioForm />} /> */}
+           
           {/* <Route path='/prueba' element={<Pruebas />} /> */}
         </Route>
       </Routes>
